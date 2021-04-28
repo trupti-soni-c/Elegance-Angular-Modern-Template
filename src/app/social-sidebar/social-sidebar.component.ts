1be1d9a3da8081061620abb1853a7fae256d842a
trupti-soni-c <sonitrupti701@gmail.com>
2021-04-28 18:26:08 +0530

    updated Angular Modern template

---
+++ src/app/social-sidebar/social-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-sidebar',
+  templateUrl: './social-sidebar.component.html',
+  styleUrls: ['./social-sidebar.component.css']
+})
+export class SocialSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
