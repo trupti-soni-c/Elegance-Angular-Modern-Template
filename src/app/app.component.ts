79ea03c990ea37bbc75aba95dd481a615a126b43
trupti-soni-c <sonitrupti701@gmail.com>
2021-04-28 16:44:36 +0530

    Angular Modern template

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ele-final-site-convert-into-angular';
+}
