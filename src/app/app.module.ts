71e920831fd3776cbfa3b355cb88c244b0b54503
trupti-soni-c <sonitrupti701@gmail.com>
2021-04-29 15:32:32 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SocialSidebarComponent } from './social-sidebar/social-sidebar.component';
 
+import { HomeComponent } from './components/home/home.component';
+
+import { ServicesComponent } from './components/services/services.component';
+import { MySkillsComponent } from './components/my-skills/my-skills.component';
+import { MyWorkComponent } from './components/my-work/my-work.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialSidebarComponent
+    SocialSidebarComponent,
+    HomeComponent,
+  
+    ServicesComponent,
+    MySkillsComponent,
+    MyWorkComponent,
+    TestimonialsComponent,
+    ContactMeComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
